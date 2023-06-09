bc6fd2780dace70bd8bbfb7bd7d42179a183f63d
bohndiggin <bohndiggin@gmail.com>
2023-06-09 12:50:25 -0600

    initial commit

---
+++ disp.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from utils import *
+
+
+class MainWindow:
+    def __init__(self, root) -> None:
+        self.root = root
---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+from disp import *
+
+v_num = 0.001
+
+root = Tk()
+root.title(f"email_crafter {v_num}")
+root.minsize(900, 700)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+def main():
+    window = MainWindow(root=root)
+
+if __name__ == '__main__':
+    main()
+    root.mainloop()
