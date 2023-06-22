48b71dc74047811e9333aca6a0f0929fc247c82b
bohndiggin <bohndiggin@gmail.com>
2023-06-22 09:45:05 -0600

    further work on UI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from utils import *
 
 from PyQt6.QtWidgets import (
     QApplication, QDialog, QMainWindow, QMessageBox
@@ -14,22 +15,31 @@ class Window(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-        # self.connectSignalsSlots()
+        self.connectSignalsSlots()
 
-    # def connectSignalsSlots(self):
-    #     self.action_Exit.triggered.connect(self.close)
-    #     self.action_Find_Replace.triggered.connect(self.findAndReplace)
-    #     self.action_About.triggered.connect(self.about)
+    def connectSignalsSlots(self):
+        self.actionOpen_Set.triggered.connect(self.placeholder_command)
+        self.actionSave_Set.triggered.connect(self.placeholder_command)
+        self.actionSave_Set_As.triggered.connect(self.placeholder_command)
+        self.actionLoad_Batch.triggered.connect(self.placeholder_command)
+        self.actionWrite_Emails.triggered.connect(self.placeholder_command)
+        
+        self.actionAdd_Document_Type.triggered.connect(self.placeholder_command)
+        self.actionAdd_Document_Set.triggered.connect(self.placeholder_command)
+        self.actionAdd_Author_Style.triggered.connect(self.placeholder_command)
+        self.actionAssign_Author_Style.triggered.connect(self.placeholder_command)
+        self.actionHook_Up_API.triggered.connect(self.placeholder_command)
+        # self.action.triggered.connect(self.placeholder_command)
+        
+        self.pushButtonAttach.clicked.connect(self.placeholder_command)
+        self.pushButtonPrev.clicked.connect(self.placeholder_command)
+        self.pushButtonSend.clicked.connect(self.placeholder_command)
+        self.pushButtonNext.clicked.connect(self.placeholder_command)
+        # self.pushButtonSend.clicked.connect(self.placeholder_command)
 
-    # def about(self):
-    #     QMessageBox.about(
-    #         self,
-    #         "About Sample Editor",
-    #         "<p>A sample text editor app built with:</p>"
-    #         "<p>- PyQt</p>"
-    #         "<p>- Qt Designer</p>"
-    #         "<p>- Python</p>",
-    #     )
+    def placeholder_command(self):
+        print('event triggered')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
