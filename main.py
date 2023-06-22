12f6bea548447d989fd71f2f0a724b1d2cffd877
bohndiggin <bohndiggin@gmail.com>
2023-06-22 09:22:25 -0600

    switched to qt6

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-from tkinter import *
-from tkinter import ttk
-from disp import *
+import sys
 
-v_num = 0.01
+from PyQt6.QtWidgets import (
+    QApplication, QDialog, QMainWindow, QMessageBox
+)
+from PyQt6.uic import loadUi
 
-root = Tk()
-root.title(f"email_crafter {v_num}")
-root.minsize(900, 700)
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+from ui import Ui_MainWindow
 
 def main():
-    window = MainWindow(root=root)
+    window = Ui_MainWindow()
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        # self.connectSignalsSlots()
+
+    # def connectSignalsSlots(self):
+    #     self.action_Exit.triggered.connect(self.close)
+    #     self.action_Find_Replace.triggered.connect(self.findAndReplace)
+    #     self.action_About.triggered.connect(self.about)
+
+    # def about(self):
+    #     QMessageBox.about(
+    #         self,
+    #         "About Sample Editor",
+    #         "<p>A sample text editor app built with:</p>"
+    #         "<p>- PyQt</p>"
+    #         "<p>- Qt Designer</p>"
+    #         "<p>- Python</p>",
+    #     )
 
 if __name__ == '__main__':
-    main()
-    root.mainloop()
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec())
